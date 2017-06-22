db12bd5e655775c1a26acbcdd850991bff1f076b
Bob H jiang <bob.h.jiang@hsbc.com.cn>
2017-06-22 20:20:58 +0800

    add charts

--- app/scripts/weather/main.controller.js
+++ app/scripts/weather/main.controller.js
@@ -1,19 +1,47 @@
 angular.module('openWeathers').controller('mainCtrl', ['$scope', 'citiyInfo', function($scope, citiyInfo) {
+	var showSeaLevel = function(time,data){
+		console.log(time,data)
+		var myChart = echarts.init(document.getElementById('chart'));
+		var option = {
+			title: {
+				text: ' sea level in the next 5 days'
+			},
+			tootip:{},
+			legend: {
+				data: ['Sea level']
+			},
+			xAxis: {
+				data: time
+			},
+			yAxis: {},
+			series:[{
+				name: 'sea level',
+				type: 'bar',
+				data: data
+			}]
+		}
+		myChart.setOption(option);
+	};
+
 	$scope.queryDetailById = function(id){
 		citiyInfo.query(id).then(function(data){
 			$scope.data = data.data.list;
 			console.log($scope.data);
-			var arr = [];
+			var date = [];
+			var seaLevel = [];
 			data.data.list.forEach(function(value){
 				var hour = new Date(value.dt_txt).getHours();
 				if(hour === 9) {
-					arr.push(value.pressure )
+					console.log(value.dt_txt)
+					date.push(value.dt_txt)
+					console.log(value.main.sea_level)
+					seaLevel.push(value.main.sea_level)
 				}
 			});
 			// sea level
-			console.log(arr)
+			showSeaLevel(date, seaLevel)
 		});
-	}
+	};
 
 	$scope.$on('showCityDetail', function(event,data) {
 		$scope.queryDetailById(data);	
--- app/scripts/weather/main.model.js
+++ app/scripts/weather/main.model.js
@@ -2,7 +2,7 @@
 angular.module('openWeathers').factory('citiyInfo', function($http){
 		var model = {};
     	model.query = function(id){
-    		var url = 'http://api.openweathermap.org/data/2.5/forecast?cnt=5&id=' + id + '&appid='+ appid;
+    		var url = 'http://api.openweathermap.org/data/2.5/forecast?cnt=35&id=' + id + '&appid='+ appid;
 
  			return $http.get(url);
     	};
